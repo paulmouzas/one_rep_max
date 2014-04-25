a3aa92e2e9c4d734e784a5d8bc8ae3facda16857
unknown <paul.mouzas@gmail.com>
2014-04-25 13:06:31 -0400

    Add form validation, sytle

--- app/models.py
+++ app/models.py
@@ -1,10 +1,10 @@
-from wtforms import Form, TextField, validators
-from wtforms.fields import SelectField
+from wtforms import Form, BooleanField, TextField, PasswordField, validators, IntegerField
+
 class Options(Form):
     #formula = SelectField('Formulas', 
     #                choices=[('epley','Epley'),('brzycki','Brzycki'),
     #                         ('lombardi', 'Lombardi'),('mayhew', 'Mayhew'),
     #                         ('oconner', "O'conner",), ('wathan', 'Wathan'),
     #                         ('lander', 'Lander')])
-    weight = TextField('weight',[validators.Required()])
-    reps = TextField('reps',[validators.Required()])
+    weight = IntegerField('weight', [validators.Required(), validators.NumberRange(min=20, max=500)])
+    reps = IntegerField('reps', [validators.Required(), validators.NumberRange(min=2, max=20)])
--- app/views.py
+++ app/views.py
@@ -1,28 +1,31 @@
-from flask import render_template
+from flask import render_template, redirect, flash
 from app import app
 from app.models import Options
 from flask import request
 from formulas import *
 import json
 
-options = Options()
-
 @app.route('/')
 def index():
-
+    form = Options(request.form)
     return render_template("index.html",
         title = 'Home',
-        options = options,)
+        form = form,
+        L = [0,0,0,0,0,0,0])
 
-@app.route('/', methods=['POST'])
+@app.route('/results', methods=['POST'])
 def results():
-    w = request.form['weight']
-    r = request.form['reps']
-    w, r = float(w), float(r)
+    form = Options(request.form)
+    if form.validate():
     
-    L = [epley(w,r),brzycki(w,r),lombardi(w,r), mayhew(w,r), lander(w,r), oconner(w,r), wathan(w,r)]
-    return render_template("index.html",
-        L = json.dumps(L),
-        options = options,
-        w = w,
-        r = r)
+        w = form.weight.data
+        r = form.reps.data
+        w, r = float(w), float(r)
+        L = [epley(w,r),brzycki(w,r),lombardi(w,r), mayhew(w,r), lander(w,r), oconner(w,r), wathan(w,r)]
+        return render_template("index.html",
+            L = json.dumps(L),
+            form = form,
+            w = w,
+            r = r)
+    flash('For Weight enter a number between 20-100 lb and for Reps between 2-10')
+    return redirect('/')
